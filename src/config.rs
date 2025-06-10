9319277ce297106c6a8bc2db3e41e69fe09591a8
tozehu <tozehu@126.com>
2025-06-10 10:39:04 +0800

    fix id key 1.4.2

--- src/config.rs
+++ src/config.rs
@@ -107,7 +107,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["10.82.1.22"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RS_PUB_KEY: &str = "xfdsfsd32131";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
